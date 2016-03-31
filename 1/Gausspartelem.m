7066ec13f36128c3358a27c0a3b449b7d0fe81fd
kamfor <kfforex@gmial.com>
2016-03-31 23:51:43 +0200

    script

--- 1/Gausspartelem.m
+++ 1/Gausspartelem.m
@@ -8,46 +8,31 @@
 
 clear
 
+for i= 0:10
+
+for j= 1:3
+
 t = cputime; 
 
-[A,b] = create_matrix(100,3); 
+[A,b] = create_matrix(10*2^i,j);
 
 [LU,P] = lucw(A);
 
-n = size(b)(1,1)
-
-b = P*b;
-
-%macierz trójkątna dolna Ly = Pb
-
-y(1,1) = b(1,1);
-
-for i = 2:n
-
-  s = b(i,1);
-  
-  for j = 1:i-1
-    s = s - LU(i,j)*y(j,1);
-  endfor
-  
-  y(i,1) = s;
-  
-endfor
-%macierz trójkątna górna Ux = y
-
-
-x(n,1) = y(n,1)/LU(n,n);
-
-for i = n-1:-1:1
-
-  p = y(i,1);
-  
-  for j = i+1:n
-    p = p - LU(i,j)*x(j,1);
-  endfor
-  
-  x(i,1) = p/LU(i,i);
-  
-endfor
+x = lufx(LU,P,b);
 
 m = cputime-t
+
+if (m>60) 
+  break; 
+  
+endif 
+
+endfor
+
+if (m>60) 
+  break; 
+  
+endif 
+
+endfor
+
---
+++ 1/lufx.m
@@ -0,0 +1,39 @@
+function [x] = lufx (LU,P,b)
+
+n = size(b)(1,1)
+
+b = P*b;
+
+%macierz trójkątna dolna Ly = Pb
+
+y(1,1) = b(1,1);
+
+for i = 2:n
+
+  s = b(i,1);
+  
+  for j = 1:i-1
+    s = s - LU(i,j)*y(j,1);
+  endfor
+  
+  y(i,1) = s;
+  
+endfor
+%macierz trójkątna górna Ux = y
+
+
+x(n,1) = y(n,1)/LU(n,n);
+
+for i = n-1:-1:1
+
+  p = y(i,1);
+  
+  for j = i+1:n
+    p = p - LU(i,j)*x(j,1);
+  endfor
+  
+  x(i,1) = p/LU(i,i);
+  
+endfor
+
+endfunction

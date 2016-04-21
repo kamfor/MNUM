145fd14312f1cadb5fcd07abb16a51719fef59d6
kamfor <kfforex@gmail.com>
2016-04-21 18:14:13 +0200

    eignoshift

--- 2/eignoshift.m
+++ 2/eignoshift.m
@@ -1,4 +1,15 @@
 %funkcja wyznaczjaca wartosci wlasne metoda qr bez przesuniec
-function [D] = eignoshift (A)
+function [D] = eignoshift (A, prec, it)
+
+  n = size(A,1); 
+  i = 1; 
+  while i <= imax & max(max(A-diag(diag(A)))) > prec
+    [Q1,R1] = qrgsm(A);
+    A = R1*Q1; 
+  end
+  if i > it 
+    error('przekrczono maksymalna liczbe iteracji');
+  end
+  D = diag(a);  
 
 end

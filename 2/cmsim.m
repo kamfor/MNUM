ef12fcbdb528c7abcb9d25371e23fb7728596691
kamfor <kfforex@gmail.com>
2016-04-22 22:11:54 +0200

    partsfinisherd1

--- 2/cmsim.m
+++ 2/cmsim.m
@@ -1,4 +1,7 @@
 %funkcja tworzaca symetryczne macierze o wymiarze n
 function [A] = cmsim (n)
 
+  B = rand(n); 
+  A = B*B'; 
+
 end
--- 2/cmunsim.m
+++ 2/cmunsim.m
@@ -1,4 +1,6 @@
 %funkcja tworzaca niesymetryczne macierze o wymiarze n
 function [A] = cmunsim (n)
 
+  A = 10*rand(n); 
+
 end

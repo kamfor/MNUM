952c250e94da54987f06a186921bfad5b89a66e2
kamfor <kfforex@gmail.com>
2016-04-25 00:22:32 +0200

    finished

--- 2/firstscript.m
+++ 2/firstscript.m
@@ -5,7 +5,7 @@
   Z = [5 10 20];
   
   %macierz symetryczna
-  fprintf(F, '###Macierz symetryczna###\n\n');
+  fprintf(F, '<<<Macierz symetryczna>>>\n\n');
   for i=1:3;
     Gtimen = 0; %zmienne do wyliczania średnich wartosci
     Gitern = 0; 
@@ -16,7 +16,6 @@
     Gprecs = 0; 
     Gvalids = 0;
     Gteig = 0; 
-    Geigprec = 0; 
     for j=1:30
       A = cmsim(Z(i));
       [R,timen,itn,validn] = eignoshift(A,0.00001,1000);% metoda bez przesuniec
@@ -25,54 +24,51 @@
       E = eig(A);
       teig = toc(eigstart);
       Gteig = Gteig + teig;
-      Geigprec = Geigprec+norm(E);
       Gtimen = Gtimen + timen;
       Gitern = Gitern + itn; 
-      Gprecn = Gprecn + norm(R);
+      Gprecn = Gprecn + norm(R-E);
       if validn == 1
         Gvalidn = Gvalidn + 1; % zliczanie poprawnych rozwiazan
       end
       Gtimes = Gtimes + times;
       Giters = Giters + its; 
-      Gprecs = Gprecs + norm(S);
+      Gprecs = Gprecs + norm(S-E);
       if valids == 1 
         Gvalids = Gvalids + 1;
       end
     end
     Gtimen = Gtimen/Gvalidn; 
-    Gitern = Gitern/Gvalidn;
+    Gitern = Gitern/Gvalidn;      [S,times,its,valids] = eigshift(A,0.00001,1000); % metoda z przesunieciami
+
     Gprecn = Gprecn/Gvalidn;
     Gtimes = Gtimes/Gvalids; 
     Giters = Giters/Gvalids;
     Gprecs = Gprecs/Gvalids;
     Gteig = Gteig/30; 
-    Geigprec = Geigprec/30;
-    fprintf(F, '###Liczba rownan %d ###\n',Z(i));
-    fprintf(F, '###Macierz symetryczna bez przesuniec###\n');
+    fprintf(F, '<<<Liczba rownan %d >>>\n',Z(i));
+    fprintf(F, '<<<Macierz symetryczna bez przesuniec>>>\n');
     fprintf(F, 'Sredni czas wykonania: %d\n',Gtimen);
     fprintf(F, 'Srednia ilosc iteracji: %d\n',Gitern);
     fprintf(F, 'Ilosc zakonczonych: %d\n',Gvalidn);
-    fprintf(F, 'Sredni blad: %d\n\n',Gprecn);
+    fprintf(F, 'Srednia roznica miedzy eig(): %d\n\n',Gprecn);
     
-    fprintf(F, '###Macierz symetryczna z przesunieciami###\n');
+    fprintf(F, '<<<Macierz symetryczna z przesunieciami>>>\n');
     fprintf(F, 'Sredni czas wykonania: %d\n',Gtimes);
     fprintf(F, 'Srednia ilosc iteracji: %d\n',Giters);
     fprintf(F, 'Ilosc zakonczonych: %d\n',Gvalids);
-    fprintf(F, 'Sredni blad: %d\n\n',Gprecs);
+    fprintf(F, 'Srednia roznica miedzy eig(): %d\n\n',Gprecs);
     
-    fprintf(F, '###Wyniki dla funkcji eig()###\n');
+    fprintf(F, '<<<Wyniki dla funkcji eig()>>>\n');
     fprintf(F, 'Sredni czas wykonania: %d\n',Gteig);
-    fprintf(F, 'Srednia blad: %d\n\n',Geigprec);
   end
   % macierz niesymetryczna
-  fprintf(F, '###Macierz niesymetryczna###\n\n');
+  fprintf(F, '<<<Macierz niesymetryczna>>>\n\n');
   for i=1:3;
     Gtimes = 0; 
     Giters = 0; 
     Gprecs = 0; 
     Gvalids = 0;
-    Gteig = 0; 
-    Geigprec = 0; 
+    Gteig = 0;  
     for j=1:30
       A = cmunsim(Z(i)); 
       [S,times,its,valids] = eigshift(A,0.00001,1000);
@@ -80,11 +76,10 @@
       E = eig(A);
       teig = toc(eigstart);
       Gteig = Gteig + teig;
-      Geigprec = Geigprec+norm(E);
       if valids == 1
         Gtimes = Gtimes + times; 
         Giters = Giters + its; 
-        Gprecs = Gprecs + norm(S); 
+        Gprecs = Gprecs + norm(S-E); 
         Gvalids = Gvalids + 1;
       end
     end
@@ -92,18 +87,17 @@
     Giters = Giters/Gvalids;
     Gprecs = Gprecs/Gvalids;
     Gteig = Gteig/30; 
-    Geigprec = Geigprec/30;
     
-    fprintf(F, '###Liczba rownan %d ###\n',Z(i));
-    fprintf(F, '###Macierz niesymetryczna z przesunieciami###\n');
+    fprintf(F, '<<<Liczba rownan %d >>>\n',Z(i));
+    fprintf(F, '<<<Macierz niesymetryczna z przesunieciami>>>\n');
     fprintf(F, 'Sredni czas wykonania: %d\n',Gtimes);
     fprintf(F, 'Srednia ilosc iteracji: %d\n',Giters);
     fprintf(F, 'Ilosc zakonczonych: %d\n',Gvalids);
-    fprintf(F, 'Sredni blad: %d\n\n',Gprecs);
+    fprintf(F, 'Srednia roznica miedzy eig(): %d\n\n',Gprecs);
     
-    fprintf(F, '###Wyniki dla funkcji eig()###\n');
+    fprintf(F, '<<<Wyniki dla funkcji eig()>>>\n');
     fprintf(F, 'Sredni czas wykonania: %d\n',Gteig);
-    fprintf(F, 'Srednia blad: %d\n\n',Geigprec);
   end
+  
   fclose(F);
    

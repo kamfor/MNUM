7d8aa1564968c9e90a62514ec75b26486cfbb7a9
kamfor <kfforex@gmail.com>
2016-04-28 11:41:46 +0200

    bisectionstart

--- 3/bisection.m
+++ 3/bisection.m
@@ -1,1 +1,10 @@
 %funkcja wyznaczajaca zera funkcji metoda bisekcji
+function [zeropoints] = bisection(fun,l,r,iter)
+%Dane wejsciowe:	l.r - lewai prawa sterona przedzialu poszukiwan
+%			fun - funkcja 
+%			iter - maksymalna liczba uteracji
+%Dane wyjsciowe: 	zerospoints - wektor miejsc zerowych funkcji na danym przedziale
+end 
+	
+
+		

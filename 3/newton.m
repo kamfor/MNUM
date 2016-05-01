fba983f2db1019d925541e15de2da5a8754e3aab
kamfor <kfforex@gmail.com>
2016-05-01 20:59:11 +0200

    ds

--- 3/newton.m
+++ 3/newton.m
@@ -1,2 +1,7 @@
 %funkcja obliczajaca zera funkcji metoda Newtona
-
+function [zeropoints] = newton(fun,l,r,iter)
+%Dane wejsciowe:	l.r - lewai prawa sterona przedzialu poszukiwan
+%			fun - funkcja 
+%			iter - maksymalna liczba uteracji
+%Dane wyjsciowe: 	zerospoints - wektor miejsc zerowych funkcji na danym przedziale
+end 
--- 3/secant.m
+++ 3/secant.m
@@ -1,1 +1,7 @@
 %funkcja obliczajaca zera funkcji metoda siecznych
+function [zeropoints] = secant(fun,l,r,iter)
+%Dane wejsciowe:	l.r - lewai prawa sterona przedzialu poszukiwan
+%			fun - funkcja 
+%			iter - maksymalna liczba uteracji
+%Dane wyjsciowe: 	zerospoints - wektor miejsc zerowych funkcji na danym przedziale
+end 

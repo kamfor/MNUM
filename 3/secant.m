754d51e25c5833825a886a145fefbff4fdd3f4cd
kamfor <kfforex@gmail.com>
2016-05-04 20:17:40 +0200

    secant

--- 3/secant.m
+++ 3/secant.m
@@ -1,7 +1,29 @@
 %funkcja obliczajaca zera funkcji metoda siecznych
-function [zeropoints] = secant(fun,l,r,iter)
+function [zeropoint] = secant(fun,l,r,iter)
 %Dane wejsciowe:	l.r - lewai prawa sterona przedzialu poszukiwan
 %			fun - funkcja 
 %			iter - maksymalna liczba uteracji
 %Dane wyjsciowe: 	zerospoints - wektor miejsc zerowych funkcji na danym przedziale
-end 
+  older = l;   old = r;
+  folder = feval(fun,older);
+  for i = 2 : iter
+    fold = feval(fun,old);
+    dx = fold * ( old - older ) / ( fold - folder );
+    new = old - dx;
+    if ( nargout == 0 )
+      disp ( sprintf ( ’\t\t %3d \t %.15f \n’, i, new ) )
+    end
+    if ( abs(dx) < TOL )
+    if ( nargout == 1 )
+    zeropoint = new;
+    end
+    return
+    else
+    older = old;
+    old = new;
+    folder = fold;
+    end
+  end
+  disp(’Osiagnieta maksymalna liczba iteracji’)
+  if ( nargout == 1 ) zeropoint = new;
+  end

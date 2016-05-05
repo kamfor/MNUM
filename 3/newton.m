ede9a35c24ec20d00e82be0d6c336ae86ea8b79e
kamfor <kfforex@gmail.com>
2016-05-05 09:01:02 +0200

    newton

--- 3/newton.m
+++ 3/newton.m
@@ -1,7 +1,25 @@
 %funkcja obliczajaca zera funkcji metoda Newtona
-function [zeropoints] = newton(fun,l,r,iter)
+function [zeropoints] = newton(fun,l,iter)
 %Dane wejsciowe:	l.r - lewai prawa sterona przedzialu poszukiwan
 %			fun - funkcja 
 %			iter - maksymalna liczba uteracji
 %Dane wyjsciowe: 	zerospoints - wektor miejsc zerowych funkcji na danym przedziale
-end 
+
+  for i = 1 : iter
+    [fold, fpold] = feval(fun,l);
+    dx = fold / fpold;
+    l = l - dx;
+    if ( nargout == 0 )
+      disp ( sprintf ( '(p. Newtona)  \t\t %3d \t %.10f ', i, l ) )
+    end
+    if ( nargout == 1 )
+    y = x0;
+    return
+    end
+  end
+  disp(’Osiagnieta maksymalna liczba iteracji’)
+  if ( nargout == 1 )
+   zeropoint = x0;
+  end
+end
+  

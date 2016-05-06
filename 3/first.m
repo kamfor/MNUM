33454982e9795614dd26542288239b8fb6d580ea
kamfor <kfforex@gmail.com>
2016-05-06 22:34:55 +0200

    plota

--- 3/first.m
+++ 3/first.m
@@ -10,4 +10,20 @@ figure
 plot(x, y, 'b','LineWidth', 2)
 grid on
 % Set the axis limits
-axis([-5 5 -1.5 1.5])
+axis([-5 5 -150 150])
+
+n=10; 
+x1 = -5; 
+x2 = -5; 
+
+%wyznaczanie przedziałów izolacji
+for j=1:n
+  if fun(x1)*fun(x2)<0
+    a = x1; 
+    b = x2; 
+  elseif abs(fun(x1))<abs(fun(x2))
+    x1 = x1+1.1*(x1-x2); 
+  else
+    x2 = x2+1.1*(x2-x1); 
+  end
+end 

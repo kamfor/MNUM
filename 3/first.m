b56c06c2a2dfee8a59dbf3460e092bae3e2c700d
kamfor <kfforex@gmail.com>
2016-05-06 23:09:58 +0200

    debugit

--- 3/first.m
+++ 3/first.m
@@ -1,29 +1,33 @@
 %Realizacja zadania 1
 clear; 
-%Generowanie wylresu funkcji aby sprawdzić poprwaność otrzymanych rozwiązań 
-% Define values for x, y1, and y2
+%Generowanie wykresu funkcji aby sprawdzic poprwanosc otrzymanych rozwiazan 
 x  = -5: .1 : 5;
-y = 1.4*sin(x)-e.^(x)+6*(x)-0.5; 
 
 % Plot y vs. x (blue, solid)
-figure
-plot(x, y, 'b','LineWidth', 2)
-grid on
-% Set the axis limits
-axis([-5 5 -150 150])
+%figure
+%plot(x, fun(x), 'b','LineWidth', 2)
+%grid on
+%axis([-5 5 -150 150])
 
-n=10; 
-x1 = -5; 
-x2 = -5; 
+n=1000; 
+x1 = 1; 
+x2 = 5; 
 
-%wyznaczanie przedziałów izolacji
+%wyznaczanie przedzialow izolacji
 for j=1:n
   if fun(x1)*fun(x2)<0
-    a = x1; 
-    b = x2; 
+    a = x1 
+    b = x2 
+    bisection('fun',a,b,100);
+    secant('fun',a,b,100) ;
+    newton('fun',a,100); 
+    x1 = x2 
   elseif abs(fun(x1))<abs(fun(x2))
-    x1 = x1+1.1*(x1-x2); 
+    x1 = x1+1.1*(x1-x2)
   else
-    x2 = x2+1.1*(x2-x1); 
+    x2 = x2+1.1*(x2-x1)
+  end
+  if(x1>5)
+    %break; wyjscie z petli po przekroczeniu przedzialu
   end
 end 
---
+++ 3/fun.m
@@ -0,0 +1,3 @@
+function y = fun (x)
+y = 1.4*sin(x)-e.^(x)+6*(x)-0.5; 
+end

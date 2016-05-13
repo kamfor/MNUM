c81f4a581383bd33697c883ad0d57455e2a5108e
kamfor <kfforex@gmail.com>
2016-05-13 13:50:08 +0200

    dfsf;'

---
+++ 3/df.m
@@ -0,0 +1,11 @@
+function [value] = df(x,n)
+%DF funkcja zwracajaca wyanaczona alanlitycznie pochodna wielomianu
+%danego w zadaniu. 
+%x - argument
+%n  - 0-wartosac funkcji,1-pierwsza pochodna,2-druga pochodna
+
+
+
+
+end
+

fa4dff6f788eca5816318f938d94bc556eebb710
kamfor <kfforex@gmail.com>
2016-06-03 18:24:11 +0200

    soidf

--- 4/rk4dynamic.m
+++ 4/rk4dynamic.m
@@ -1,7 +1,20 @@
-function [ output_args ] = untitled4( input_args )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+function [Y] = rk4dynamic(x,timelimit,stp)
+%RK4 Rozwiazanie ukladu metoda Rungego-Kutty czwartego rzędu ze zmiennym
+%krokiem
+%x - stan poczatkowy
+%timelimit - zakres czasu
+%step - rozmiar kroku
 
-
+    Y = zeros(ceil(timelimit/stp),3); %macierz stanów x1, x2 i czasu
+    hstp=stp/2; %połowa kroku
+    for i = 1:(ceil(timelimit/stp))
+        Y(i,3) = i*stp; %zapisanie czasu próbki
+        k1 = f_x(x); %pochodna w punkcie y(xn)
+        k2 = f_x(x+hstp*k1); %pochodna w punkcie y(xn+step/2*k1)
+        k3 = f_x(x+hstp*k2); %pochodna w punkcie y(xn+step/2*k2)
+        k4 = f_x(x+stp*k3); %pochodna w punkcie y(xn+step*k3)
+        x=x+(1/6)*stp*(k1+2*k2+2*k3+k4); %obliczenie następnego punktu
+        Y(i,1:2) = x; %zapisanie punktu do wektora
+        
+    end
 end
-
--- 4/rk4static.m
+++ 4/rk4static.m
@@ -5,25 +5,16 @@ function [Y] = rk4static(x,timelimit,stp)
 %step - rozmiar kroku
 
 
-    Y = zeros(ceil(timelimit/stp),3); 
-    %macierz stanów x1, x2 i czasu
-    hstp=stp/2; 
-    %połowa kroku
+    Y = zeros(ceil(timelimit/stp),3); %macierz stanów x1, x2 i czasu
+    hstp=stp/2; %połowa kroku
     for i = 1:(ceil(timelimit/stp))
-        Y(i,3) = i*stp; 
-        %zapisanie czasu próbki
-        k1 = f_x(x);
-        %pochodna w punkcie y(xn)
-        k2 = f_x(x+hstp*k1); 
-        %pochodna w punkcie y(xn+step/2*k1)
-        k3 = f_x(x+hstp*k2); 
-        %pochodna w punkcie y(xn+step/2*k2)
-        k4 = f_x(x+stp*k3); 
-        %pochodna w punkcie y(xn+step*k3)
-        x=x+(1/6)*stp*(k1+2*k2+2*k3+k4); 
-        %obliczenie następnego punktu
-        Y(i,1) = x(1); 
-        Y(i,2) = x(2); 
+        Y(i,3) = i*stp;  %zapisanie czasu próbki
+        k1 = f_x(x); %pochodna w punkcie y(xn)
+        k2 = f_x(x+hstp*k1); %pochodna w punkcie y(xn+step/2*k1)        Y(i,2) = x(2);
+        k3 = f_x(x+hstp*k2); %pochodna w punkcie y(xn+step/2*k2)
+        k4 = f_x(x+stp*k3); %pochodna w punkcie y(xn+step*k3)
+        x=x+(1/6)*stp*(k1+2*k2+2*k3+k4); %obliczenie następnego punktu
+        Y(i,1:2) = x;  
         %zapisanie punktu do wektora
     end
 end
